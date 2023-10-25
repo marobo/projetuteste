4e4eff51db6cb49126adb57b8f0829b9069e9e9b
onorio <onorio@catalpa.io>
2023-10-25 10:22:14 +0900

    fix models

--- appteste/models.py
+++ appteste/models.py
@@ -2,8 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=50)
     content = models.TextField()
-    created_on = models.DateTimeField(date_now=:qa
-                                      )
-    modified_on = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    modified_on = models.DateTimeField(auto_now=True)
