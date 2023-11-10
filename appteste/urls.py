b9317bb4a7e2843fa75dcde3dd1a9bbe5420e1ee
onorio <onorio@catalpa.io>
2023-11-10 16:33:26 +0900

    created post form

--- appteste/urls.py
+++ appteste/urls.py
@@ -3,5 +3,6 @@ from appteste import views
 
 urlpatterns = [
     path('', views.index, name="homepage"),
+    path('create', views.post_create, name="post_create"),
     path('posts', views.post_list, name="post_list"),
 ]
--- appteste/views.py
+++ appteste/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from .models import Post
+from .forms import PostForm
 
 
 def index(request):
     my_dict = {"insert_me": "I am from views.py"}
     return render(request,'index.html',context=my_dict)
+
+def post_create(request):
+    form = PostForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            form.save()
+            return redirect ('post_list')
+    context = {
+        "form":form
+    }
+    return render(request, 'appteste/post_create.html', context)
 
 
 def post_list(request):
